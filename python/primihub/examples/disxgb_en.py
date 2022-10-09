82ea276d28c76bd417516b05bd7003e89575832d
xus <xus@xusdeMacBook-Pro.local>
2022-10-09 16:20:52 +0800

    refact(song.xu):refact disxgb with proxy

--- python/primihub/examples/disxgb_en.py
+++ python/primihub/examples/disxgb_en.py
@@ -21,10 +21,10 @@ from primihub.primitive.opt_paillier_c2py_warpper import *
 # from primihub.channel.zmq_channel import IOService, Session
 from primihub.FL.proxy.proxy import ServerChannelProxy
 from primihub.FL.proxy.proxy import ClientChannelProxy
-from primihub.FL.model.xgboost.xgb_guest_en import XGB_GUEST_EN
-from primihub.FL.model.xgboost.xgb_host_en import XGB_HOST_EN
-from primihub.FL.model.xgboost.xgb_guest import XGB_GUEST
-from primihub.FL.model.xgboost.xgb_host import XGB_HOST
+# from primihub.FL.model.xgboost.xgb_guest_en import XGB_GUEST_EN
+# from primihub.FL.model.xgboost.xgb_host_en import XGB_HOST_EN
+# from primihub.FL.model.xgboost.xgb_guest import XGB_GUEST
+# from primihub.FL.model.xgboost.xgb_host import XGB_HOST
 from primihub.FL.model.evaluation.evaluation import Regression_eva
 from primihub.FL.model.evaluation.evaluation import Classification_eva
 import pandas as pd
