ead33e7703a89a3d5d38a807dc78143d61a023fd
xusong <xu15701157125@163.com>
2023-04-25 18:45:28 +0800

    feat(song.xu): refact hetero lr

--- python/primihub/FL/model/logistic_regression/hetero_lr_base.py
+++ python/primihub/FL/model/logistic_regression/hetero_lr_base.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn import metrics
 from collections import Iterable
+from primihub.new_FL.algorithm.utils.net_work import GrpcServer, GrpcServers
+from primihub.new_FL.algorithm.utils.base_xus import BaseModel
 
 
 def dloss(p, y):
@@ -45,38 +47,10 @@ def trucate_geometric_thres(x, clip_thres, variation, times=2):
     return dp_x
 
 
-class HeteroLrBase:
+class HeteroLrBase(BaseModel):
 
-    def __init__(self,
-                 learning_rate=0.01,
-                 alpha=0.0001,
-                 epochs=10,
-                 penalty="l2",
-                 batch_size=64,
-                 optimal_method=None,
-                 update_type=None,
-                 loss_type='log',
-                 random_state=2023,
-                 clip_thres=1.0,
-                 noise_variation=1.0):
-        self.learning_rate = learning_rate
-        self.alpha = alpha
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.penalty = penalty
-        self.optimal_method = optimal_method
-        self.random_state = random_state
-        self.update_type = update_type
-        self.loss_type = loss_type
-        self.clip_thres = clip_thres
-        self.noise_variation = noise_variation
-        self.theta = 0
-
-    def fit(self):
-        pass
-
-    def predict(self):
-        pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def loss(self, y_hat, y_true):
         if self.loss_type == 'log':
@@ -90,116 +64,62 @@ class HeteroLrBase:
         else:
             raise KeyError('The type is not implemented!')
 
+    def get_summary(self):
+        """
+        """
+        return {}
 
-class PlainLR:
+    def set_inputs(self):
+        # set common parameters
+        self.model = self.kwargs['common_params']['model']
+        self.task_name = self.kwargs['common_params']['task_name']
+        self.learning_rate = self.kwargs['common_params']['learning_rate']
+        self.alpha = self.kwargs['common_params']['alpha']
+        self.epochs = self.kwargs['common_params']['epochs']
+        self.penalty = self.kwargs['common_params']['penalty']
+        self.optimal_method = self.kwargs['common_params']['optimal_method']
+        self.momentum = self.kwargs['common_params']['momentum']
+        self.random_state = self.kwargs['common_params']['random_state']
+        self.scale_type = self.kwargs['common_params']['scale_type']
+        self.batch_size = self.kwargs['common_params']['batch_size']
+        self.sample_method = self.kwargs['common_params']['sample_method']
+        self.sample_ratio = self.kwargs['common_params']['sample_ratio']
+        self.loss_type = self.kwargs['common_params']['loss_type']
+        self.prev_grad = self.kwargs['common_params']['prev_grad']
+        self.model_path = self.kwargs['common_params']['model_path']
+        self.metric_path = self.kwargs['common_params']['metric_path']
+        self.model_pred = self.kwargs['common_params']['model_pred']
 
-    def __init__(self,
-                 learning_rate=0.01,
-                 alpha=0.0001,
-                 epochs=10,
-                 penalty="l2",
-                 batch_size=64,
-                 optimal_method=None,
-                 update_type=None,
-                 loss_type='log',
-                 random_state=2023):
-        self.learning_rate = learning_rate
-        self.alpha = alpha
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.penalty = penalty
-        self.optimal_method = optimal_method
-        self.random_state = random_state
-        self.update_type = update_type
-        self.loss_type = loss_type
-        self.theta = 0
+        # set role special parameters
+        self.role_params = self.kwargs['role_params']
 
-    def add_intercept(self, x):
-        intercept = np.ones((x.shape[0], 1))
-        return np.concatenate((intercept, x), axis=1)
+        # set party node information
+        self.node_info = self.kwargs['node_info']
 
-    def sigmoid(self, x):
-        return 1.0 / (1 + np.exp(-x))
+        # set other parameters
+        self.other_params = self.kwargs['other_params']
 
-    def predict_prob(self, x):
-        return self.sigmoid(np.dot(x, self.theta))
+    def run(self):
+        pass
 
-    def predict(self, x):
-        preds = self.predict_prob(x)
-        preds[preds <= 0.5] = 0
-        preds[preds > 0.5] = 1
+    def get_outputs(self):
+        return dict()
 
-        return preds
+    def get_status(self):
+        return {}
 
-    def gradient(self, x, y):
-        h = self.predict_prob(x)
-        if self.penalty == "l2":
-            grad = (np.dot(x.T, (h - y)) / x.shape[0] + self.alpha * self.theta
-                   )  #/ x.shape[0]
-        elif self.penalty == "l1":
-            raise ValueError("It's not implemented now!")
 
-        else:
-            grad = np.dot(x.T, (h - y)) / x.shape[0]  #/ x.shape[0]
+class HeteroLrHost(HeteroLrBase):
 
-        return grad
-
-    def update_lr(self, current_epoch, type="sqrt"):
-        if type == "sqrt":
-            self.learning_rate /= np.sqrt(current_epoch + 1)
-        else:
-            typw = np.sqrt(1.0 / np.sqrt(self.alpha))
-            initial_eta0 = typw / max(1.0, dloss(-typw, 1.0))
-            optimal_init = 1.0 / (initial_eta0 * self.alpha)
-
-            self.learning_rate = 1.0 / (self.alpha *
-                                        (optimal_init + current_epoch + 1))
-
-    def simple_gd(self, x, y):
-        grad = self.gradient(x, y)
-        self.theta -= self.learning_rate * grad
-        # print("======", self.theta, grad, self.learning_rate)
-
-    def batch_gd(self, x, y):
-        for batch_x, bathc_y in batch_yield(x, y, self.batch_size):
-            grad = self.gradient(batch_x, bathc_y)
-
-            self.theta -= self.learning_rate * grad
-
-    def fit(self, x, y):
-        x = self.add_intercept(x)
-        if self.batch_size < 0:
-            self.batch_size = x.shape[0]
-
-        np.random.seed(self.random_state)
-        self.theta = np.random.rand(x.shape[1])
-        total_loss = []
-
-        for i in range(self.epochs):
-            self.update_lr(i, type=self.update_type)
-
-            if self.optimal_method == "simple":
-                self.simple_gd(x, y)
-
-            else:
-                self.batch_gd(x, y)
-
-            print("current iteration and theta", i, self.theta)
-
-            y_hat = np.dot(x, self.theta)
-            current_loss = self.loss(y_hat, y)
-            total_loss.append(current_loss)
-            # print("current iteration and loss", i, current_loss)
-        print("loss", total_loss)
-
-    def loss(self, y_hat, y_true):
-        if self.loss_type == 'log':
-            y_prob = self.sigmoid(y_hat)
-
-            return metrics.log_loss(y_true, y_prob)
-
-        elif self.loss == "squarederror":
-            return metrics.mean_squared_error(
-                y_true, y_hat)  # mse don't activate inputs
-        else:
-            raise KeyError('The type is not implemented!')
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.channel = GrpcServers(local_role=self.other_params.party_name,
+                                   remote_roles=self.role_params['neighbors'],
+                                   party_info=self.node_info,
+                                   task_info=self.other_params.task_info)
+        self.add_noise = self.role_params['add_noise']
+        self.tol = self.role_params['tol']
+        self.data_set = self.role_params['data_set']
+        self.selected_column = self.role_params['selected_column']
+        self.label = self.role_params['label']
+        self.n_iter_no_change = self.role_params['n_iter_no_change']
